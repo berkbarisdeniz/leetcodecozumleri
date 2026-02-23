4315e867801d6a5afbe62e98db2c2a5ef32ef2bd
Baris Deniz <berkbaris.dnz@gmail.com>
2026-02-23 16:36:15 +0300

    kaydetmemisim. düzeltme

--- easy/14-longest-common-prefix.py
+++ easy/14-longest-common-prefix.py
@@ -1,5 +1,14 @@
-strs = ["baris","baban","bakın"]
+class Solution(object):
+    def longestCommonPrefix(self, strs): 
+        prefix = strs[0]
+        for word in strs[1:len(strs):]:
+            temp_prefix = ""
+            for char1, char2 in zip(word,prefix):
+                if char1 == char2 :
+                    temp_prefix += char1
+                else:
+                    break
+            prefix=temp_prefix
+        return prefix 
 
 
-
-print(zip(strs))
